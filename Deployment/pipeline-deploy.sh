963f332b1c99ab8bb50d8638a64483d85ded88b4
carlos.escalona <carlos.escalona@T21>
2022-01-07 15:25:19 -0300

    basic pipline build

---
+++ Deployment/pipeline-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -z "$1" || -z "$2" ]]
+then
+    exit 1
+fi
+
+echo "paso"
+
+sam deploy --template-file ./pipeline.yml --s3-bucket $1 --s3-prefix send-me-an-image-pipeline --region sa-east-1 --capabilities CAPABILITY_NAMED_IAM --stack-name send-me-an-image-pipeline --parameter-overrides ParameterKey=ARTIFACTSBUCKET,ParameterValue="$2" --no-fail-on-empty-changeset
